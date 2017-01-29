07f424655b037e065503fbdabb8bcbe44c1eafc1
Alex Lostak <alex@cubeatx.com>
2017-01-28 18:46:55 -0600

    initial setup

---
+++ EE460N_Lab_01/EE460N_Lab_01/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  EE460N_Lab_01
+//
+//  Created by Alex Lostak on 1/28/17.
+//  Copyright © 2017 Alex Lostak. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
