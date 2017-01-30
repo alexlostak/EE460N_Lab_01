f63cbbe98479f74846f1e7c4584886f4bd79d1f5
Alex Lostak <alex@cubeatx.com>
2017-01-30 13:12:11 -0600

    Forgotten changes

--- EE460N_Lab_01/EE460N_Lab_01/instructions.c
+++ EE460N_Lab_01/EE460N_Lab_01/instructions.c
@@ -12,8 +12,9 @@
 
 
 uint16_t reg_to_uint16_t (char* reg) {
-    
-    return 1;
+    uint16_t char_value;
+    char_value = reg[1] - 48; //ASCII value of 0;
+    return char_value;
 }
 
 instr_t* instr_new (char* lLabel, uint16_t lOpcode, char* lArg1, char* lArg2, char* lArg3, char* lArg4) {
@@ -35,18 +36,21 @@ instr_add_imm_t* instr_add_imm_t_new (instr_t* instr) {
     //assign numbers to fields in add struct
     //ADD R1, R1, #5
     //convert arg1
+    //variable declaration
     uint16_t arg1;
     //convert arg2
     uint16_t arg2;
     //convert imm5
-    uint16_t imm5 = toNum(instr->arg3);
-    instr_add_imm_t* i = malloc(sizeof(instr_add_imm_t));
+    uint16_t imm5;
+    instr_add_imm_t* i;
+    arg1 = reg_to_uint16_t(instr->arg1);
+    arg2 = reg_to_uint16_t(instr->arg2);
+    imm5 = toNum(instr->arg3);
+    i = malloc(sizeof(instr_add_imm_t));
     i->opcode = instr->opcode;
-    arg1 = instr->arg1[1] - 49;
     i->A = 1;
     i->imm5 = imm5;
-    
-    
-    
+    i->DR1 = arg1;
+    i->SR1 = arg2;
     return i;
 }
